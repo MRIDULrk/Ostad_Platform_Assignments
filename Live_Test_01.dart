a5377a47aafd46c1b99d7dc46f1b49642ec54ab9
Mridul Ranjan <100429674+MRIDULrk@users.noreply.github.com>
2024-03-16 13:38:49 +0600

    Initial Commit

--- Live_Test_01.dart
+++ Live_Test_01.dart
@@ -1,58 +1,46 @@
 void main() {
-  Map<int, String> fruitlist = {
-
-    1: 'Apple',
-    2: 'Banana',
-    3: 'Grapes'
+  List<dynamic> apple = ['Apple', 'Red', 2.5];
+  List<dynamic> mango = ['Mango', 'yellow', 9.5];
+  List<dynamic> grasp = ['Grasp', 'purple', 10.5];
+  Map<int, dynamic> fruitlist= {
+    1: apple,
+    2: mango,
+    3: grasp
   };
 
-  print(fruitlist);
-  displayFruitDetails(fruitlist);
-  applyPriceDiscount(fruitlist,1);
+  display(fruitlist);
+  discountPrice(fruitlist,50);
 
 }
 
+void display(Map<int,dynamic> fruits){
 
-void displayFruitDetails(Map<int,String> fruits) {
+  for(int i=1;i<=fruits.keys.length;i++) {
 
- if(fruits[1]=='Apple'){
+    dynamic value = fruits[i];
+    String name = value[0];
+    String color = value[1];
+    double price = value[2];
 
-   print('Name: Apple, Color: Red, Price: 2.5');
- }
- if(fruits[2]=='Banana'){
+    print('Fruits Name: '+name +', Colour:'+color +', Price:'+price.toString());
 
-   print('Name: Banana, Color: Yellow, Price: 10');
- }
- if(fruits[3]=='Grapes'){
-
-   print('Name: Grapes, Color: Purple, Price: 2.5');
- }
+  }
 
 }
 
+void discountPrice(Map<int,dynamic> fruits,int discountRate){
 
-void applyPriceDiscount(Map<int,String> fruits,int discount){
 
-  if(fruits[1]=='Apple'){
-    int price1=2.5-discount;
+  print('After '+discountRate.toString()+'% Discount The Details is:');
 
-    print('Name: Apple, Color: Red');
-    print(price1);
+  for(int i=1;i<=fruits.keys.length;i++) {
+
+    dynamic value = fruits[i];
+    String name = value[0];
+    String color = value[1];
+    double price = value[2]*(discountRate/100);
+
+    print('Fruits Name: '+name +', Colour:'+color +', Price:'+price.toString());
+
   }
-  if(fruits[2]=='Banana'){
-    int price2=10-discount;
-
-    print('Name: Banana, Color: Yellow,price:);
-        print(price2);
-  }
-  if(fruits[3]=='Grapes'){
-    int price3=10-discount;
-
-    print('Name: Grapes, Color: Purple,);
-        print(price3);
-  }
-
-
-
 }
-
